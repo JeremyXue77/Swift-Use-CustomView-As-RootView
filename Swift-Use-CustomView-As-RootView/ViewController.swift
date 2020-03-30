f64f8f9aafa31f18e469abdf1eb5379e1286e225
Jeremy Xue <gg831006@gmail.com>
2020-03-30 10:01:53 +0800

    🎉 (Project): Init git

---
+++ Swift-Use-CustomView-As-RootView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Use-CustomView-As-RootView
+//
+//  Created by Jeremy Xue on 2020/3/30.
+//  Copyright © 2020 jeremyxue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
