0c7bfb88b27e8e4319849b84ea71cb1a56af0a8d
Jeremy Xue <gg831006@gmail.com>
2020-03-30 10:16:55 +0800

    🚩 (ViewController): Set up basic features in ViewController

--- Swift-Use-CustomView-As-RootView/ViewController.swift
+++ Swift-Use-CustomView-As-RootView/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlet
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var lightSwitch: UISwitch!
+    @IBOutlet weak var fontSizeStepper: UIStepper!
+    
+    // MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setFontSizeStepperDefaultValue()
+    }
+    
+    // MARK: Setting methods
+    private func setFontSizeStepperDefaultValue() {
+        fontSizeStepper.value = Double(messageLabel.font.pointSize)
     }
 
-
+    // MARK: IBAction
+    @IBAction func toggleLightMode(_ sender: UISwitch) {
+        messageLabel.textColor = !sender.isOn ? .black : .white
+        view.backgroundColor = sender.isOn ? .black : .white
+    }
+    
+    @IBAction func changeMessageLabelFontSize(_ sender: UIStepper) {
+        let fontSize = CGFloat(sender.value)
+        messageLabel.font = UIFont.systemFont(ofSize: fontSize)
+    }
 }
+
+
 
